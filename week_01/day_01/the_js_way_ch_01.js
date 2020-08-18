f1d6a1274d8c5308f6dc72dc9ad6798248933cd4
Roger David Cruz <roger.david.cruz@gmail.com>
2020-08-17 23:12:10 -0400

    js review

---
+++ week_01/day_01/the_js_way_ch_01.js
@@ -0,0 +1,29 @@
+// Week_01 - the js way chapter 01
+
+//Minimalistic Calculator
+//Write a program that displays the results of adding, subtracting,
+//multiplying and dividing 6 by 3.
+
+console.log(6/3)
+//output: 2
+
+console.log(6*3)
+//output: 18
+
+console.log(6-3)
+//output: 3
+
+console.log(6+3)
+//output: 9
+
+//Values prediction for following program.
+//Note: a program is lines of code.
+
+console.log(4+5);
+//output: 9
+
+console.log("4 + 5");
+//output: "4 + 5"
+
+console.log("4" + "5");
+//output: "45"
